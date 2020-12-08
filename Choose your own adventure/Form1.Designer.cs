926573686e8eccdca73e03e414d883577bf6cdb2
Adrian Racho <adrirach172@outlook.com>
2020-12-08 14:37:54 -0500

    e

--- Choose your own adventure/Form1.Designer.cs
+++ Choose your own adventure/Form1.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 434);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keydown_1);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Choose your own adventure/Form1.cs
+++ Choose your own adventure/Form1.cs
@@ -7,14 +7,126 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Media;
 namespace Choose_your_own_adventure
 {
     public partial class Form1 : Form
     {
+        int scene = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+       
+        private void Minecraft_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void keydown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)      
+            {
+                if (scene == 0)
+                {
+                    scene = 1;
+                }
+                else if (scene == 1)
+                {
+                    scene = 4;
+                }
+                else if (scene == 2)
+                {
+                    scene = 7;
+                }
+                else if (scene == 3)
+                {
+                    scene = 5;
+                }
+                else if (scene == 4)
+                {
+                    scene = 11;
+                }
+                else if (scene == 5)
+                {
+                    scene = 11;
+                }
+                else if (scene == 6)
+                {
+                    scene = 11;
+                }
+                else if (scene == 7)
+                {
+                    scene = 11;
+                }
+                else if (scene == 8)
+                {
+                    scene =9;
+                }
+                else if (scene == 9)
+                {
+                    scene = 11;
+                }
+                else if (scene == 10)
+                {
+                    scene = 11;
+                }              
+                else if (e.KeyCode == Keys.D) 
+                {
+                    if (scene == 0)
+                    {
+                        scene = 1;
+                    }
+
+                    else if (scene == 1)
+                    {
+                        scene = 2;
+                    }
+                    else if (scene == 2)
+                    {
+                        scene = 6;
+                    }
+                    else if (scene == 3)
+                    {
+                        scene = 6;
+                    }
+                    else if (scene == 4)
+                    {
+                        scene = 11;
+                    }
+                    else if (scene == 5)
+                    {
+                        scene = 11;
+                    }
+                    else if (scene == 6)
+                    {
+                        scene = 11;
+                    }
+                    else if (scene == 7)
+                    {
+                        scene = 11;
+                    }
+                    else if (scene == 8)
+                    {
+                        scene = 20;
+                    }
+                    else if (scene == 9)
+                    {
+                        scene = 11;
+                    }
+                    else if (scene == 10)
+                    {
+                        scene = 11;
+                    }
+
+                }
+            }
         }
     }
 }
